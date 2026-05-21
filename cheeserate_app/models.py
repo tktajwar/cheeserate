fd9c70eacf9ab2ddb402f50b0dfc381b8c1a4e27
Takey Tajwar <tajwar.earth@pm.me>
2026-05-21 19:52:48 +0600

    create item table

--- cheeserate_app/models.py
+++ cheeserate_app/models.py
@@ -1,6 +1,18 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
 
 class User(models.Model):
     username = models.CharField(max_length=64, unique=True)
+
+class Item(models.Model):
+    score_avg = models.FloatField(
+        validators=[
+            MinValueValidator(-1.0),
+            MaxValueValidator(+1.0),
+        ],
+    )
+    score_sum = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
