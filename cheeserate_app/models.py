b8ab55fe305309bbede7a67a273c15f74ded62af
Takey Tajwar <tajwar.earth@pm.me>
2026-05-24 08:25:35 +0600

    implement __str__ for Item table

--- cheeserate_app/models.py
+++ cheeserate_app/models.py
@@ -21,6 +21,9 @@ class Item(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
 class Rating(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
