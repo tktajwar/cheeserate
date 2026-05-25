89efc6d8efcb703d858340290dfba91b80c0ea2f
Takey Tajwar <tajwar.earth@pm.me>
2026-05-25 21:46:37 +0600

    use render shortcut for root page view

--- cheeserate_app/views.py
+++ cheeserate_app/views.py
@@ -8,7 +8,5 @@ from .models import Item, Film
 
 def root(request):
     latest_items = [ item for item in Film.objects.order_by("-pk") ]
-    print(latest_items[0].get_absolute_url())
-    template = loader.get_template("cheeserate/index.html")
     context = {"latest_items": latest_items}
-    return HttpResponse(template.render(context, request))
+    return render(request, "cheeserate/index.html", context)
