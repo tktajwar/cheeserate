7cf8d083892a7d52660010ff1304f764df3a410f
Takey Tajwar <tajwar.earth@pm.me>
2026-05-30 18:46:18 +0600

    register cheeserate models to admin page

--- cheeserate_app/admin.py
+++ cheeserate_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import User, Crew, Film, CrewStarringFilm, CrewDirectedFilm
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Crew)
+admin.site.register(Film)
+admin.site.register(CrewStarringFilm)
+admin.site.register(CrewDirectedFilm)
